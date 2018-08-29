f62220354403b4c5bb8b8a41f2241e1feeb66993
Samuil9999 <freelancr@ukr.net>
2018-08-29 23:51:22 +0300

    Created RInput class

--- interrupt_test.py
+++ interrupt_test.py
@@ -5,14 +5,11 @@ INPUT0 = 16
 INPUT1 = 20
 INPUT2 = 21
 
-#counter_0 = 0
-#lasttime_0 = time.time()
-
-#GPIO.setup(INPUT0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(INPUT1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.setup(INPUT2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-#def processPulse(channel, status):
+def playWarning():
+	print("Warning!!!")
+	
+def playAudio(number):
+	print("Playing audio number " + str(number))
 
 def gpioCallback(channel):
 	if(channel == INPUT0):
@@ -27,21 +24,41 @@ def gpioCallback(channel):
 		print("channel:" + str(channel) + ", counter:" + str(input2.counter))
 		
 class RInput:
-	def __init__(self, number):
+	def __init__(self, number, pulselength):
 		self.number = number
+		self.pulselength = pulselength
 		self.counter = 0
 		self.lasttime = time.time()
+		#self.delta = 0
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.add_event_detect(number, GPIO.FALLING, callback=gpioCallback, bouncetime=200)
+		GPIO.add_event_detect(number, GPIO.BOTH, callback=gpioCallback, bouncetime=100)
 		print("RInput:" + str(number) + " created.")
 	
 	def processInterrupt(self):
-		print("time.time()-lasttime:{}".format(self.lasttime-time.time()))
-		self.lasttime = time.time()
+		self.delta = time.time() - self.lasttime
 		self.counter += 1
+
+		print("time.time() - lasttime:{}".format(time.time() - self.lasttime))
+
+		if self.delta > self.pulselength:
+			print("Total number of pulses:{}".format(self.counter))
+			self.counter = 0
+
+		self.lasttime = time.time()
+		
+class RInput0(RInput):
+	def processInterrupt(self):
+		self.delta = time.time() - self.lasttime
+		print("time.time() - lasttime:{}".format(time.time()-self.lasttime))
+		if self.delta > self.pulselength:
+			print("Total number of pulses:{}".format(self.counter + 1))
+			self.counter = 0
+		else:
+			self.counter += 1
+		self.lasttime = time.time()
 	
-input0 = RInput(INPUT0);
+input0 = RInput(INPUT0, 2.5);
 
 while True:
 	time.sleep(1)
